8f319e199f064b27aed03b4ab7333cb9c460fb8f
Andres Loeh <andres@well-typed.com>
2021-03-04 16:54:56 +0100

    Final version.

--- src/SOP1.hs
+++ src/SOP1.hs
@@ -1,6 +1,84 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE UndecidableSuperClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
 module SOP1 where
 
 import Data.Kind
 import Data.Proxy
 import GHC.TypeLits
 
+data NP :: (a -> Type) -> [a] -> Type where
+  Nil  :: NP f '[]
+  (:*) :: f x -> NP f xs -> NP f (x : xs)
+
+infixr 5 :*
+
+deriving instance All (Compose Show f) xs => Show (NP f xs)
+
+type family All f xs :: Constraint where
+  All f '[]      = ()
+  All f (x : xs) = (f x, All f xs)
+
+class (f (g x)) => Compose f g x
+instance (f (g x)) => Compose f g x
+
+newtype I a = I a
+  deriving Show
+
+newtype K a b = K a
+  deriving Show
+
+example1 :: NP I '[ Int, Char, Bool ]
+example1 = I 3 :* I 'x' :* I True :* Nil
+
+example2 :: NP (K String) '[ Int, Char, Bool ]
+example2 = K "foo" :* K "bar" :* K "baz" :* Nil
+
+map_NP :: (forall x . f x -> g x) -> NP f xs -> NP g xs
+map_NP f Nil       = Nil
+map_NP f (x :* xs) = f x :* map_NP f xs
+
+cmap_NP :: All c xs => Proxy c -> (forall x . c x => f x -> g x) -> NP f xs -> NP g xs
+cmap_NP _ f Nil       = Nil
+cmap_NP p f (x :* xs) = f x :* cmap_NP p f xs
+
+collapse_NP :: NP (K a) xs -> [a]
+collapse_NP Nil = []
+collapse_NP (K x :* xs) = x : collapse_NP xs
+
+data NS :: (a -> Type) -> [a] -> Type where
+  Z :: f x -> NS f (x : xs)
+  S :: NS f xs -> NS f (x : xs)
+
+deriving instance All (Compose Show f) xs => Show (NS f xs)
+
+example3 :: NS I '[ Int, Char, Bool ]
+example3 = S (Z (I 'a'))
+
+map_NS :: (forall x . f x -> g x) -> NS f xs -> NS g xs
+map_NS f (Z x) = Z (f x)
+map_NS f (S s) = S (map_NS f s)
+
+cmap_NS :: All c xs => Proxy c -> (forall x . c x => f x -> g x) -> NS f xs -> NS g xs
+cmap_NS _ f (Z x) = Z (f x)
+cmap_NS p f (S s) = S (cmap_NS p f s)
+
+newtype SOP f xss = SOP (NS (NP f) xss)
+
+example4 :: SOP I '[ '[ Int ], '[ Char, Bool ] ]
+example4 = SOP (S (Z (I 'x' :* I True :* Nil)))
+
+map_SOP :: (forall x . f x -> g x) -> SOP f xss -> SOP g xss
+map_SOP f (SOP sop) = SOP (map_NS (map_NP f) sop)
+
+-- cmap_SOP :: All (All c) xss => Proxy c -> (forall x . c x => f x -> g x) -> SOP f xss -> SOP g xss
+-- cmap_SOP = undefined
--- src/SOP2.hs
+++ src/SOP2.hs
@@ -25,8 +25,15 @@ import Data.Kind
 import Data.SOP
 import Data.SOP.NP
 import Data.SOP.NS
-import qualified GHC.Generics as GHC
-import Generics.SOP
+-- import qualified GHC.Generics as GHC
+-- import Generics.SOP
+
+class Generic a where
+
+  type Code a :: [[Type]]
+
+  from :: a -> SOP I (Code a)
+  to :: SOP I (Code a) -> a
 
 data Person =
   MkPerson
@@ -46,7 +53,6 @@ andres :: Person
 andres =
   MkPerson "Andres" "Haskell" 8
 
-{-
 instance Generic Person where
 
   type Code Person = '[ '[ String, String, Int ] ]
@@ -56,9 +62,7 @@ instance Generic Person where
 
   to :: SOP I (Code Person) -> Person
   to (SOP (Z (I name :* I lang :* I nr :* Nil))) = MkPerson name lang nr
--}
 
-{-
 instance Generic Language where
 
   type Code Language = '[ '[], '[], '[] ]
@@ -72,5 +76,26 @@ instance Generic Language where
   to (SOP (Z Nil))         = Haskell
   to (SOP (S (Z Nil)))     = OCaml
   to (SOP (S (S (Z Nil)))) = Agda
--}
+
+gToRecord :: (Generic a, Code a ~ '[ xs ], All ToField xs) => a -> Record
+gToRecord =
+  record . collapse_NP . cmap_NP (Proxy @ToField) (\ (I x) -> K (toField x)) . unZ . unSOP . from
+
+instance ToRecord Person where
+  toRecord = gToRecord
+
+indices :: All Top xs => NP (K Int) xs
+indices =
+  ana_NP (\ (K i) -> (K i, K (i + 1))) (K 0)
+
+gParseRecord :: (Generic a, Code a ~ '[ xs ], All FromField xs) => Record -> Parser a
+gParseRecord r =
+  fmap (to . SOP . Z) (sequence_NP (cmap_NP (Proxy @FromField) (\ (K i) -> r .! i) indices))
+
+instance FromRecord Person where
+  parseRecord = gParseRecord
+
+data Tree = Leaf | Node Tree Tree
+
+data Perfect a = Zero a | Succ (Perfect (a, a))
 
